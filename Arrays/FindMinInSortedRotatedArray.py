377b7835fc8970a969ba4d833cb6cfb9be51cb21
Manju S V <manjusvijaykumar@gmail.com>
2018-01-04 17:50:34 +0530

    Adding comments

--- Arrays/FindMinInSortedRotatedArray.py
+++ Arrays/FindMinInSortedRotatedArray.py
@@ -2,13 +2,14 @@
 # @Author: Manju S V
 # @Date:   2018-01-04 17:37:40
 # @Last Modified by:   Manju S V
-# @Last Modified time: 2018-01-04 17:43:12
+# @Last Modified time: 2018-01-04 17:49:44
 
 class Solution:
     def _findMin(self, A, low, high):
+        # if no rotation at all return first element
         if A[high] > A[low]:
-            # No rotation at all return first element
             return A[0]
+
         # if there is only one element in array
         if low == high:
             return A[low]
@@ -27,16 +28,15 @@ class Solution:
             return self._findMin(A, low, mid - 1)
         return self._findMin(A, mid + 1, high)
             
-    # @param A : tuple of integers
-    # @return an integer
+    # Function to find min element in sorted rotated array
     def findMin(self, A):
         low = 0
         high = len(A) - 1
         return self._findMin(A, low, high)
 
-# create a linked list with user input
+# create an object of class
 obj = Solution()
 A = [ 6, 7, 8, 0, 1, 2 ]
-min1 = obj.findMin(A)
+min_ele = obj.findMin(A)
 
-print "Min Element in array : ", min1
+print "Min Element in array : ", min_ele
